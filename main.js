fe02fc5991196e2fee26fff2b20374cd6fca95bb
Murat-developer <78870265+Murat-developer@users.noreply.github.com>
2022-05-24 12:22:27 +0100

    Reducing an Arrays

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
-// Chaining an Arrays
+// Reducing an Arrays
 
 const numbers = [1, -1, 2, 3];
 
-const items = numbers
-  .filter((n) => n >= 0)
-  .map((n) => ({ value: n }))
-  .filter((obj) => obj.value > 1)
-  .map((obj) => obj.value);
+const sum = numbers.reduce(
+  (accumulator, currentValue) => accumulator + currentValue,
+);
 
-console.log(items);
+console.log(sum);
