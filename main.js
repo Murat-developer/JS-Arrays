808cda788a75a51e18f89f7a23047dbaf2539aad
Murat-developer <78870265+Murat-developer@users.noreply.github.com>
2022-05-24 12:09:42 +0100

    Mapping an Arrays

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
-// Filter Method
+// Mappping an Array
 
 const numbers = [1, -1, 2, 3];
 
-const number = numbers.filter((value) => {
-  return value >= 0;
-});
+const number = numbers.filter((value) => value >= 0);
+
+const mapping = numbers.map((value) => ({ value: value }));
+
+console.log(mapping);
